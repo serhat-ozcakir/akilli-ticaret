67cf828ec1834e8d9889b64c564e3782c80ce88b
serhat-ozcakir <serhatcakir87@gmail.com>
2022-01-23 21:38:38 +0300

    update comit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,10 +50,10 @@ const Navbar = () => {
           </div>
           <div className="p-1">
             <a href="" className="navbar-brand mx-2">
-              <i class="bi bi-person-circle fs-4"></i>
+              <i class="bi bi-person-circle"></i>
             </a>
             <a href="" className="navbar-brand ">
-              <i class="bi bi-cart-fill fs-4"></i>
+              <i class="bi bi-cart-fill"></i>
             </a>
           </div>
         </div>
